8f30e38ee704e72d6e89fbe3a29c1257f82feea5
androds <andrei_simeonov@hotmail.com>
2020-01-25 08:45:34 +0200

    Added repositories

---
+++ PCConfigurationTool/PCCOnfiguration.Data/Implementations/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PCConfiguration.Data.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCConfiguration.Data.Implementations.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T: class
+    {
+        private PcDbContext _context = null;
+        private DbSet<T> table = null;
+        public GenericRepository()
+        {
+            this._context = new PcDbContext();
+            table = _context.Set<T>();
+        }
+        public GenericRepository(PcDbContext _context)
+        {
+            this._context = _context;
+            table = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public void Create(T obj)
+        {
+            table.Add(obj);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ PCConfigurationTool/PCCOnfiguration.Data/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCConfiguration.Data.Interfaces.Repositories
+{
+    public interface IGenericRepository <T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        void Create(T obj);
+    }
+}
